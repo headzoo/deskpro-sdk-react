fdcbbedfcf2dc79c272f178106d380f45841c8dd
headzoo <sean@headzoo.io>
2017-10-06 13:09:54 -0400

    Adds tests for route class

---
+++ tests/utils/route.test.js
@@ -0,0 +1,30 @@
+import Route from '../../src/utils/route';
+import * as types from '../../src/actions/actionTypes';
+
+test('Route create location and param properties', () => {
+  const dispatch = jest.fn();
+  const data     = {
+    location: 'testing',
+    params:   { id: 1 }
+  };
+  const route = new Route(dispatch, data);
+
+  expect(route.location).toEqual(data.location);
+  expect(route.params).toEqual(data.params);
+});
+
+test('Route dispatches a location', () => {
+  const dispatch = jest.fn();
+  const data     = {
+    location: 'testing',
+    params:   {}
+  };
+  const route = new Route(dispatch, data);
+  route.to('testing2');
+
+  expect(dispatch).toBeCalledWith({
+    type:     types.SDK_TO_ROUTE,
+    location: 'testing2',
+    params:   {}
+  });
+});
